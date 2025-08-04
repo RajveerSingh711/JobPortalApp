97af54c0e99ae72464a257d24b870268ae652ff4
RajveerSingh711 <rajveer032020@gmail.com>
2025-08-04 16:22:25 +0530

    Job page designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Login from "./components/auth/Login"
 import SignUp from "./components/auth/SignUp"
 import Home from "./components/Home"
+import Jobs from "./components/Jobs"
 
 const appRouter=createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const appRouter=createBrowserRouter([
   {
     path:'/signup',
     element:<SignUp/>
+  },
+  {
+    path:"/jobs",
+    element:<Jobs/>
   }
 ])
 
