efd23b405a0b9c5654dff4885bf611e3676d4f71
RajveerSingh711 <rajveer032020@gmail.com>
2025-08-05 20:32:33 +0530

    profile page designed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import SignUp from "./components/auth/SignUp"
 import Home from "./components/Home"
 import Jobs from "./components/Jobs"
 import Browse from "./components/Browse"
+import Profile from "./components/Profile"
 
 const appRouter=createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const appRouter=createBrowserRouter([
   {
     path:"/browse",
     element:<Browse/>
+  },
+  {
+    path:"/profile",
+    element:<Profile/>
   }
 ])
 
---
+++ frontend/src/components/AppliedJobTable.jsx
@@ -0,0 +1,42 @@
+import { Badge } from "./ui/badge";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+
+const AppliedJobTable = () => {
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of your applied jobs</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Date</TableHead>
+            <TableHead>Job Role</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {[1, 2].map((item, index) => (
+            <TableRow key={index}>
+              <TableCell>05-08-2025</TableCell>
+              <TableCell>Full Stack Developer</TableCell>
+              <TableCell>Bajaj Finserv</TableCell>
+              <TableCell className="text-right">
+                <Badge>Selected</Badge>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default AppliedJobTable;
